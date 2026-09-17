3d66bcbfe4de00be5065bb81fa70b6e9b51f6b2a
Srinithi NV <srinithinv@gmail.com>
2026-09-17 20:04:38 +0530

    Remove custom cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/layout/Header';
 import { Footer } from './components/layout/Footer';
-import { CustomCursor } from './components/ui/CustomCursor';
 import { Chatbot } from './components/ui/Chatbot';
 import { HomePage } from './pages/HomePage';
 import { CourseDetailsPage } from './pages/CourseDetailsPage';
@@ -15,8 +14,7 @@ function App() {
     <BookingProvider>
       <Router>
         <ScrollToTop />
-        <div className="bg-white min-h-screen selection:bg-[var(--color-brand)] selection:text-white cursor-none">
-          <CustomCursor />
+        <div className="bg-white min-h-screen selection:bg-[var(--color-brand)] selection:text-white">
           <Header />
           <Routes>
             <Route path="/" element={<HomePage />} />
